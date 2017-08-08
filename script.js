0749ef755bf0f603569e9d2cc45f3154e4270b3d
Stefan Surkamp <stefan.surkamp@gmx.de>
2017-08-08 12:33:28 +0200

    updated jQuery version and HTTP to HTTPS

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 function changeImageSize() {
-			var scroll = getCurrentScroll();
+	var scroll = getCurrentScroll();
 
-			if(scroll >= 350) {
-				$('#desktopHeader').css('height', 150);
-				$('.titleContainer').css('bottom', 10);
-				$('.codeSkillsIntro').css('opacity', 0);				
-			}
-			else {
-				$('#desktopHeader').height(500-scroll);
-				$('.titleContainer').css('bottom', (Math.floor(500-scroll)/2)-65);
-				$('.codeSkillsIntro').css('opacity', (Math.floor(350-scroll))/110);
-			}
+	if(scroll >= 350) {
+		$('#desktopHeader').css('height', 150);
+		$('.titleContainer').css('bottom', 10);
+		$('.codeSkillsIntro').css('opacity', 0);				
+	}
+	else {
+		$('#desktopHeader').height(500-scroll);
+		$('.titleContainer').css('bottom', (Math.floor(500-scroll)/2)-65);
+		$('.codeSkillsIntro').css('opacity', (Math.floor(350-scroll))/110);
+	}
+
+}
+
+function getCurrentScroll() {
+	return window.pageYOffset || document.documentElement.scrollTop;
+}
+
+$( document ).ready(function() {
+	if($('#desktopHeader').css('display') == 'block') {
+		var scroll = getCurrentScroll();
+		if(scroll >= 350) {
+			$('#desktopHeader').css('height', 150);
+			$('.titleContainer').css('bottom', 10);
+			$('.codeSkillsIntro').css('opacity', 0);
+		}
+		else {
+			$('#desktopHeader').height(500-scroll);
+			$('.titleContainer').css('bottom', (Math.floor(500-scroll)/2)-65);
+			$('.codeSkillsIntro').css('opacity', (Math.floor(350-scroll))/110);
 
 		}
+	}
 
-		function getCurrentScroll() {
-			return window.pageYOffset || document.documentElement.scrollTop;
-		}
+	$('.codeSkillsTitle').on('click', function(e){
+		$('html,body').animate({scrollTop:0}, '500');
+		e.preventDefault();
+	});
 
-		$( document ).ready(function() {
-			if($('#desktopHeader').css('display') == 'block') {
-				var scroll = getCurrentScroll();
-				if(scroll >= 350) {
-					$('#desktopHeader').css('height', 150);
-					$('.titleContainer').css('bottom', 10);
-					$('.codeSkillsIntro').css('opacity', 0);
-				}
-				else {
-					$('#desktopHeader').height(500-scroll);
-					$('.titleContainer').css('bottom', (Math.floor(500-scroll)/2)-65);
-					$('.codeSkillsIntro').css('opacity', (Math.floor(350-scroll))/110);
+});
 
-				}
-			}
-
-			$('.codeSkillsTitle').on('click', function(e){
-				$('html,body').animate({scrollTop:0}, '500');
-				e.preventDefault();
-			});
-
-		});
-
-		$(window).scroll(function() {
-			if($('#desktopHeader').css('display') == 'block') {
-				changeImageSize();
-			}
-		});
+$(window).scroll(function() {
+	if($('#desktopHeader').css('display') == 'block') {
+		changeImageSize();
+	}
+});
